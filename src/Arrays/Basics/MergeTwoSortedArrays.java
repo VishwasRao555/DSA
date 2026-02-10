02cf3adf119a3f33075821b34215c323bdd5512a
vishwas <vishwasraoch555@gmail.com>
2026-02-11 00:01:30 +0530

    Added Arrays first dsa problem

---
+++ src/Arrays/Basics/MergeTwoSortedArrays.java
@@ -0,0 +1,37 @@
+package Arrays.Basics;
+
+public class MergeTwoSortedArrays {
+    public static int[] merge(int[] a, int[] b) {
+        int n = a.length;
+        int m = b.length;
+        int[] result = new int[n + m];
+        int i = 0, j = 0, k = 0;
+        while (i < n && j < m) {
+            if (a[i] <= b[j]) {
+                result[k++] = a[i++];
+            } else {
+                result[k++] = b[j++];
+            }
+        }
+        while (i < n) {
+            result[k++] = a[i++];
+        }
+            while (j < m) {
+                result[k++] = b[j++];
+            }
+            return result;
+        }
+
+        public static void main(String[] args) {
+            int[] a = {1, 3, 5, 7};
+            int[] b = {2, 4, 6, 8};
+
+            int[] merged = merge(a, b);
+
+            for (int num : merged) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+
