80630316b9f01b65f6df2af2dde2e9640f3a70c1
vishwas <vishwasraoch555@gmail.com>
2026-03-27 01:50:00 +0530

    Adding Finding the Missing Number in An Array of 1 to N

--- src/Arrays/Standard/Find_Missing_number_in_Array_1_to_N.java
+++ src/Arrays/Standard/Find_Missing_number_in_Array_1_to_N.java
@@ -6,13 +6,13 @@ public class Find_Missing_number_in_Array_1_to_N {
         System.out.println("Missing Number is :"+Missing_number(nums));
     }
     static int Missing_number(int[] nums){
-        int N=nums.length+1;
-         int Total_sum=N*(N+1)/2;
+         int N=nums.length+1;//Total Length of the Array Including the Missing Number
+         int Total_sum=N*(N+1)/2;//Sum of N natural Number
          int sum=0;
          for(int i:nums){
-             sum+=i;
+             sum+=i;//Sum of the Array excluding the missing Number
          }
-         int missing_num=Total_sum-sum;
+         int missing_num=Total_sum-sum;//x=sum-(sum-x)
          return missing_num;
     }
 }
